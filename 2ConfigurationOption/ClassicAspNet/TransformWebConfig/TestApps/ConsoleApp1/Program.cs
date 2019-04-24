12eeadd388b97ecbca37e9cdcd1aa39f2129c46c
Murat Cabuk <muratcabuk@live.com>
2019-04-24 08:13:01 +0300

    Added webconfig transformation example for classic aspnet app

---
+++ 2ConfigurationOption/ClassicAspNet/TransformWebConfig/TestApps/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        [Obsolete]
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(ConfigurationSettings.AppSettings["Name"].ToString());
+
+            Console.ReadKey();
+            
+
+
+
+        }
+    }
+}
---
+++ 2ConfigurationOption/ClassicAspNet/TransformWebConfig/TestApps/WebApplication1/Default.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var rrr = ConfigurationManager.AppSettings.Get("Name");
+
+            Response.Write(rrr);
+
+        }
+    }
+}
